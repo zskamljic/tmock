f628f8491bd3305e322833480020d2978134ebc6
zskamljic <zan.skamljic@equaleyes.com>
2020-04-11 13:04:05 +0200

    feat: add sha1 docs

--- sha1/src/lib.rs
+++ sha1/src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests;
 
+/// Utility macro to help with adding with overflows.
 macro_rules! add_with_mask {
     ($x:expr) => {
         $x
@@ -13,32 +14,40 @@ macro_rules! add_with_mask {
     }
 }
 
+/// Execute sha1 on a `&str` and returns a `String` of hex encoded data.
 pub fn sha1_str_as_str(string: &str) -> String {
     let mut sha = Sha1::new();
     sha.update_str(string);
     sha.hex_digest()
 }
 
+/// Execute sha1 on a `&str` and returns an array of 20 bytes.
 pub fn sha1_str_as_bytes(string: &str) -> [u8; 20] {
     let mut sha = Sha1::new();
     sha.update_str(string);
     sha.digest()
 }
 
+/// Execute sha1 on array of bytes and returns an array of 20 bytes.
 pub fn sha1_bytes_as_bytes(bytes: &[u8]) -> [u8; 20] {
     let mut sha = Sha1::new();
     sha.update(bytes);
     sha.digest()
 }
 
+/// Sha1 state
 #[derive(Default)]
 pub struct Sha1 {
+    /// the h1...h5 values
     h: [u32; 5],
+    /// The total length of the message
     message_length: u32,
+    /// Undigested bytes
     pending: Vec<u8>,
 }
 
 impl Sha1 {
+    /// Creates a new state with default vector
     pub fn new() -> Sha1 {
         Sha1 {
             h: [
@@ -53,15 +62,18 @@ impl Sha1 {
         }
     }
 
+    /// Update the state with string.
     pub fn update_str(&mut self, data: &str) {
         self.update(data.as_bytes())
     }
 
+    /// Update the state with byte array.
     pub fn update(&mut self, data: &[u8]) {
         self.pending.extend(data.iter().cloned());
         self.consume_pending();
     }
 
+    /// Processes a chunk of 64 bytes.
     // The names follow specification, changing them would hinder readability
     #[allow(clippy::many_single_char_names)]
     fn process_chunk(&mut self, chunk: Vec<u8>) {
@@ -101,6 +113,7 @@ impl Sha1 {
         self.h[4] = add_with_mask!(self.h[4], e);
     }
 
+    /// Consume the pending bytes if their length is at least 64.
     fn consume_pending(&mut self) {
         while self.pending.len() >= 64 {
             let chunk = self.pending.drain(0..64).collect();
@@ -109,6 +122,7 @@ impl Sha1 {
         }
     }
 
+    /// Finalize the hash digest.
     fn digest(&mut self) -> [u8; 20] {
         let message_byte_len = self.message_length as usize + self.pending.len();
         self.pending.push(0x80);
@@ -131,6 +145,7 @@ impl Sha1 {
         result
     }
 
+    /// Perform the digest and return it as a hex encoded `String`.
     pub fn hex_digest(&mut self) -> String {
         self.digest()
             .iter()
@@ -140,6 +155,7 @@ impl Sha1 {
     }
 }
 
+/// Fill the start of the array with chunk bytes.
 fn fill_start(words: &mut [u32; 80], chunk: Vec<u8>) {
     for i in 0..=15 {
         let bytes = [
@@ -152,6 +168,7 @@ fn fill_start(words: &mut [u32; 80], chunk: Vec<u8>) {
     }
 }
 
+/// Fill the rest of the array with the data from the start
 fn extend(words: &mut [u32; 80]) {
     for i in 16..=79 {
         words[i] = (words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16]).rotate_left(1);
