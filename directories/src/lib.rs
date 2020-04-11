5216c663ea0670026a13af57d0c3e6cc9115c38f
zskamljic <zan.skamljic@equaleyes.com>
2020-04-11 10:06:00 +0200

    feat: add docs for directories and split tests into separate file

--- directories/src/lib.rs
+++ directories/src/lib.rs
@@ -1,7 +1,17 @@
+#[cfg(test)]
+mod tests;
+
 use std::fs;
 use std::io::Result;
 use std::path::Path;
 
+/// Checks if directory exists and attempts to create one
+/// if it doesn't exist. Returns error if it was not able
+/// to create one.
+///
+/// # Arguments
+///
+/// * `path` - the path that needs to exist or create.
 pub fn must_exist(path: &str) -> Result<()> {
     let path = Path::new(path);
 
@@ -12,54 +22,4 @@ pub fn must_exist(path: &str) -> Result<()> {
     fs::create_dir(path)?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs::File;
-
-    #[test]
-    fn must_exist_suceeds_with_existing() -> Result<()> {
-        const DIR: &str = "preexisting_dir";
-
-        fs::create_dir(DIR)?;
-
-        let result = must_exist(DIR);
-
-        fs::remove_dir(DIR)?;
-
-        result
-    }
-
-    #[test]
-    fn must_exist_creates_if_able() -> Result<()> {
-        const DIR: &str = "inexisting_dir";
-
-        let result = must_exist(&DIR);
-
-        if Path::new(DIR).is_dir() {
-            fs::remove_dir(DIR)?;
-        } else {
-            panic!("Dir was not created");
-        }
-
-        result
-    }
-
-    #[test]
-    fn must_exist_fails_on_file() -> Result<()> {
-        const DIR: &str = "existing_file";
-
-        File::create(DIR)?;
-        let result = must_exist(DIR);
-
-        fs::remove_file(DIR)?;
-
-        if result.is_err() {
-            Ok(())
-        } else {
-            panic!("Did not fail with existing file");
-        }
-    }
 }
